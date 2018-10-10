4803113a6554a92fe1a81a7ab771fb80eac01f61
j.butajlo <j.butajlo@samsung.com>
2018-10-10 14:11:06 +0200

    use case executor for parametrized use cases

--- presentation/src/main/java/com/butajlo/punkbeers/rx/usecase/UseCaseExecutor.kt
+++ presentation/src/main/java/com/butajlo/punkbeers/rx/usecase/UseCaseExecutor.kt
@@ -1,5 +1,6 @@
 package com.butajlo.punkbeers.rx.usecase
 
+import com.butajlo.punkbeers.common.ParametrizedUseCase
 import com.butajlo.punkbeers.common.UseCase
 import io.reactivex.Scheduler
 import io.reactivex.Single
@@ -11,25 +12,43 @@ import javax.inject.Singleton
 class UseCaseExecutor(private val ioScheduler: Scheduler,
                       private val uiScheduler: Scheduler) {
 
-    fun <Result> async(useCase: UseCase<Single<Result>>)
-            : Builder<Result> where Result : Any {
-        return Builder(useCase, ioScheduler, uiScheduler)
-    }
+    // UseCase async
+
+    fun <Result> async(useCase: UseCase<Single<Result>>) where Result : Any
+            = Builder(useCase, ioScheduler, uiScheduler)
+
 
     class Builder<Result>(
             private val useCase: UseCase<Single<Result>>,
             private val ioScheduler: Scheduler,
             private val uiScheduler: Scheduler) where Result : Any {
 
-        fun execute(onSuccess: (Result) -> Unit, onError: (Throwable) -> Unit) : Disposable =
-            useCase.execute()
-                    .subscribeOn(ioScheduler)
-                    .observeOn(uiScheduler)
-                    .subscribeBy(
-                            onSuccess = onSuccess,
-                            onError = onError
-                    )
+        fun execute(onSuccess: (Result) -> Unit, onError: (Throwable) -> Unit): Disposable =
+                useCase.execute()
+                        .subscribeOn(ioScheduler)
+                        .observeOn(uiScheduler)
+                        .subscribeBy(
+                                onSuccess = onSuccess,
+                                onError = onError
+                        )
 
+    }
+
+    fun <P, R> async(parametrizedUseCase: ParametrizedUseCase<P, Single<R>>) where R : Any
+            = ParametrizedBuilder(parametrizedUseCase, ioScheduler, uiScheduler)
+
+    class ParametrizedBuilder<P, R>(private val useCase: ParametrizedUseCase<P, Single<R>>,
+                                    private val ioScheduler: Scheduler,
+                                    private val uiScheduler: Scheduler) where R : Any {
+
+        fun execute(params: P, onSuccess: (R) -> Unit, onError: (Throwable) -> Unit): Disposable =
+                useCase.execute(params)
+                        .subscribeOn(ioScheduler)
+                        .observeOn(uiScheduler)
+                        .subscribeBy(
+                                onSuccess = onSuccess,
+                                onError = onError
+                        )
     }
 
 }
