d7bbae24658778bcd65c75ccfc0eaa25fd97376a
j.butajlo <j.butajlo@samsung.com>
2018-10-12 15:35:12 +0200

    details fragment

---
+++ presentation/src/main/java/com/butajlo/punkbeers/screens/details/BeerDetailsModel.kt
@@ -0,0 +1,8 @@
+package com.butajlo.punkbeers.screens.details
+
+data class DetailsModel(
+        val name: String,
+        val tagLine: String,
+        val description: String,
+        val firstBrewed: String
+)
